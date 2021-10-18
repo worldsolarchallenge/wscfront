0ce98886d0cc1e78dd752505b1cd4abd091be1f5
David Snowdon <dcasnowdon@gmail.com>
2021-10-18 23:19:43 +1100

    Linting fixes.

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 
 import flask
 import flask_caching
-from flask.helpers import send_file
 
 import subprocess
 
@@ -22,7 +21,7 @@ INFLUX_HOST = os.environ.get(
 INFLUX_ORG = os.environ.get("INFLUX_ORG", "BWSC")
 INFLUX_TOKEN = os.environ.get("INFLUX_TOKEN", None)
 
-#INFLUX_BUCKET = os.environ.get("INFLUX_BUCKET", "sample")
+# INFLUX_BUCKET = os.environ.get("INFLUX_BUCKET", "sample")
 
 if not INFLUX_TOKEN:
     raise ValueError("No InfluxDB token set using INFLUX_TOKEN "
@@ -36,14 +35,17 @@ if not INFLUX_ORG:
     raise ValueError("No InfluxDB org set using INFLUX_ORG "
                      "environment variable")
 
+
 @app.route("/")
 def root():
     return app.send_static_file('front.html')
 
+
 @cache.cached(timeout=60, key_prefix="run_front")
 def run_front():
-    # Run "Front" here. 
+    # Run "Front" here.
     return subprocess.call(['Front'])
+
 
 @app.route('/front.svg')
 @app.route('/front_graph.svg')
@@ -53,11 +55,14 @@ def myfiles():
 
     print(f"Sending {flask.request.path}")
     if flask.request.path.endswith(".svg"):
-        return flask.send_file(f"results/{flask.request.path}", mimetype='image/svg+xml')
+        return flask.send_file(f"results/{flask.request.path}",
+                               mimetype='image/svg+xml')
     elif flask.request.path.endswith(".kml"):
-        return flask.send_file(f"results/{flask.request.path}", mimetype="application/vnd.google-earth.kml+xml")
+        return flask.send_file(f"results/{flask.request.path}",
+                mimetype="application/vnd.google-earth.kml+xml") # noqa
 
     raise ValueError("Un-routed file detected.")
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
